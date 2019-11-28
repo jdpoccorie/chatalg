31baffdeba041b6da7d11ac4d86ac04bb8d4fafc
J Diego Poccori Escalante <juandiego.poccori@gmail.com>
2019-11-28 11:45:07 -0500

    edit

--- index.js
+++ index.js
@@ -30,22 +30,7 @@ io.on('connection', (socket) => {
   });
 
   // when the client emits 'add user', this listens and executes
-  socket.on('add user', (username) => {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
+ 
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', () => {
@@ -62,15 +47,5 @@ io.on('connection', (socket) => {
   });
 
   // when the user disconnects.. perform this
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
+  
 });
